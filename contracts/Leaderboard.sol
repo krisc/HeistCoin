ba908e0bdf691b4d2fd4541a3f244ed57bca2d79
Kris Calabio <kriscalabio@gmail.com>
2019-04-30 09:04:37 -0700

    Leaderboard v0.1

--- contracts/Leaderboard.sol
+++ contracts/Leaderboard.sol
@@ -1,13 +1,40 @@
 pragma solidity 0.5.0;
 contract Leaderboard {
-    mapping (address => uint256) private _highscores;
+
+	struct PlayerRecord {
+        string name;
+        uint score;
+    }
+
+    mapping (address => PlayerRecord) private _highscores;
+    mapping (uint => address) public addresses;
+    uint public totalPlayers = 0;
     
-    function setHighscore(uint256 value) public returns (bool) {
-        _highscores[msg.sender] = value;
+    function setHighscore(string memory name, uint score) public returns (bool) {
+        //check if player already has a record
+        if(_highscores[msg.sender].score <= 0) {
+            _addPlayerAddress();
+        }
+        
+        _highscores[msg.sender] = PlayerRecord(name, score);
+        return true;
+    }
+    function _addPlayerAddress() private returns (bool) {
+        addresses[totalPlayers] = msg.sender;
+        _incrementPlayers();
         return true;
     }
     
-    function getHighscore() public view returns (uint256) {
-        return _highscores[msg.sender];
+    function _incrementPlayers() private returns (bool) {
+        totalPlayers++;
+        return true;
+    }
+    
+    function getName(address a) public view returns (string memory) {
+    	return _highscores[a].name;
+    }
+
+    function getHighscore(address a) public view returns (uint) {
+    	return _highscores[a].score;
     }
 }    
--- index.js
+++
@@ -1,4 +0,0 @@
-console.log("Hi");
-// const Web3 = require('web3');
-console.log("Jesus Christ Superstar");
-// console.dir(Web3);
